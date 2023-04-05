efdcb29417f86f6063cbbf276d4ec95d5c01f234
rickdeleon <ricky.deleon@cuw.edu>
2023-04-05 10:19:12 -0500

    Code from making character move from room to room

--- DungeonCrawler/Assets/script/MasterData.cs
+++ DungeonCrawler/Assets/script/MasterData.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public class MasterData
 {
     public static int count = 0;
+    public static string whereDidIComeFrom = "?";
 
 }
--- DungeonCrawler/Assets/script/playercontroller.cs
+++ DungeonCrawler/Assets/script/playercontroller.cs
@@ -9,42 +9,80 @@ public class playercontroller : MonoBehaviour
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
     public float movementSpeed = 40.0f;
+    private bool isMoving;
+    private Vector3 hold;
     // Start is called before the first frame update
     void Start()
     {
+        this.rb.position = hold;
+        print(MasterData.whereDidIComeFrom);
         this.rb = this.GetComponent<Rigidbody>();
         print(MasterData.count);
-       
+        this.isMoving = false;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Exit"))
+        {
+            if (other.gameObject == this.northExit)
+            {
+                MasterData.whereDidIComeFrom = "north";
+                hold = -1 * (GameObject.FindGameObjectWithTag("Exit").transform.position);
+            }
+            else if (other.gameObject == this.southExit)
+            {
+                MasterData.whereDidIComeFrom = "south";
+                hold = -1 * (GameObject.FindGameObjectWithTag("Exit").transform.position);
+            }
+            else if (other.gameObject == this.eastExit)
+            {
+                MasterData.whereDidIComeFrom = "east";
+                hold = -1 * (GameObject.FindGameObjectWithTag("Exit").transform.position);
+            }
+            else if (other.gameObject == this.westExit)
+            {
+                MasterData.whereDidIComeFrom = "west";
+                hold = -1 * (GameObject.FindGameObjectWithTag("Exit").transform.position);
+            }
+            print("I hit an exit");
+
+            SceneManager.LoadScene("DungeonRoom");
+            
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !isMoving)
         {
             this.rb.AddForce(this.northExit.transform.position * movementSpeed);
-            SceneManager.LoadScene("DungeonRoom");
+            this.isMoving = true;
+
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && !isMoving)
             {
 
                 this.rb.AddForce(this.westExit.transform.position * movementSpeed);
-                MasterData.count++;
-                SceneManager.LoadScene("DungeonRoom");
+                this.isMoving = true;
+                
 
             }
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) && !isMoving)
         {
             this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
-            SceneManager.LoadScene("DungeonRoom");
+            this.isMoving = true;
+
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.DownArrow) && !isMoving)
         {
             this.rb.AddForce(this.southExit.transform.position * movementSpeed);
-            SceneManager.LoadScene("DungeonRoom");
+            this.isMoving = true;
+
         }
     }
 
